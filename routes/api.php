7f2578ccc5607f6399b6aed30a6e6a2b0c93d4fa
AlanAltamiranoH1504 <altamiranohernandezalan@gmail.com>
2025-07-02 22:33:19 -0600

    feat: add token generation with sanctum - add token model - add TokenController - add sanctum library for token generation - add request validator for token generation route - group DELETE and PUT routes of UserController with sanctum middleware - TODO: correct business logic according to consent_IDs

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::post('/usuario/save', [UserController::class, 'store']);
-Route::put("/usuario/update/{id}", [UserController::class, "update"]);
-Route::delete("/usuario/delete/{id}", [UserController::class, "destroy"]);
+Route::post("/token", [\App\Http\Controllers\TokenController::class, "getToken"]);
+
+Route::middleware("auth:sanctum")->group(function () {
+    Route::put("/usuario/update/{id}", [UserController::class, "update"]);
+    Route::delete("/usuario/delete/{id}", [UserController::class, "destroy"]);
+});
